5e800d9bb7c7467d44975d2a04817879f20c2a1b
dm-sha <i@dmsha.ru>
2025-08-17 16:16:54 +0300

    Update EventumPage.tsx
    Remove link from import

--- frontend/src/pages/EventumPage.tsx
+++ frontend/src/pages/EventumPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getEventumBySlug } from "../api/eventum";
 import type { Eventum } from "../types";
 import LoadingSpinner from "../components/LoadingSpinner";
