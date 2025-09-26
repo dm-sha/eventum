998514168314807ff0ebbef1b474fbd77f4daf82
dm-sha <i@dmsha.ru>
2025-09-26 14:14:00 +0700

    add secrets

--- backend/eventum/settings.py
+++ backend/eventum/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t=syq08#$#drsr2s@e(+l39sc9f(8mjluu$*^oyzw4u&uw*^d!'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
