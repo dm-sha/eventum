1c30d1e38b72be1566adb4c2c38db40f5a9f6d45
dm-sha <i@dmsha.ru>
2025-10-04 19:56:55 +0300

    add analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { AppRouter } from './router/AppRouter';
 import { AuthProvider } from './contexts/AuthContext';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
     return (
         <AuthProvider>
             <AppRouter />
+            <Analytics />
         </AuthProvider>
     );
 }
